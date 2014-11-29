25d326eb37314ffb12eddb10b6a5161ba515b1b0
Bart Waeterschoot <bart.waeterschoot@gmail.com>
2014-11-29 13:26:11 +0100

    Added authentication error handling

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -1,21 +1,25 @@
 'use strict';
 
-app.controller('AuthCtrl', function ($scope, $location, Auth) {
-  	if (Auth.signedIn()) {
-	      $location.path('/');
-	    }
+app.controller('AuthCtrl', function ($scope, $location, Auth, user) {
+  if (user) {
+    $location.path('/');
+  }
 
-	    $scope.login = function () {
-	      Auth.login($scope.user).then(function () {
-	        $location.path('/');
-	      });
-	    };
+  $scope.login = function () {
+    Auth.login($scope.user).then(function () {
+      $location.path('/');
+    }, function (error) {
+      $scope.error = error.toString();
+    });
+  };
 
   $scope.register = function () {
     Auth.register($scope.user).then(function() {
       return Auth.login($scope.user).then(function() {
         $location.path('/');
       });
+    }, function(error) {
+      $scope.error = error.toString();
     });
   };
 });
