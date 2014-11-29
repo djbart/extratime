af75a7b2f8898ab829163a756bc9864e22c4cf81
Bart Waeterschoot <bart.waeterschoot@gmail.com>
2014-11-29 13:22:56 +0100

    Added email/pwd authentication

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,28 @@ var app = angular
 	      templateUrl: 'views/posts.html',
 	      controller: 'PostsCtrl'
 	    })
+		.when('/posts/:postId', {
+		    templateUrl: 'views/showpost.html',
+		    controller: 'PostViewCtrl'
+		  })
+		.when('/register', {
+		  templateUrl: 'views/register.html',
+		  controller: 'AuthCtrl',
+		  resolve: {
+		    user: function(Auth) {
+		      return Auth.resolveUser();
+		    }
+		  }
+		})
+		.when('/login', {
+		  templateUrl: 'views/login.html',
+		  controller: 'AuthCtrl',
+		  resolve: {
+		    user: function(Auth) {
+		      return Auth.resolveUser();
+		    }
+		  }
+		})
       .otherwise({
         redirectTo: '/'
       });
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,15 +1,9 @@
 'use strict';
 
-app.controller('PostsCtrl', function ($scope, Post) {
-  $scope.posts = Post.all;
+app.controller('PostsCtrl', function ($scope, $location, Post) {
+  	$scope.posts = Post.all;
 
-  $scope.post = {url: 'http://', 'title': ''};
-
-  $scope.submitPost = function () {
-    Post.create($scope.post).then(function () {
-      $scope.post = {url: 'http://', 'title': ''};
-    });
-  };
+	  $scope.post = {url: 'http://'};
 
   $scope.deletePost = function (post) {
     Post.delete(post);
